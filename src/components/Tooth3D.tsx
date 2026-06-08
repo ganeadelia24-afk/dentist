24f11f69c802c961833dc58ae00fa758a054112b
ganeadelia24-afk <ganeadelia24@gmail.com>
2026-06-08 22:13:17 +0300

    Remove unused React import from Tooth3D component

--- src/components/Tooth3D.tsx
+++ src/components/Tooth3D.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Float, MeshDistortMaterial, PerspectiveCamera, Environment, ContactShadows } from '@react-three/drei'
 import * as THREE from 'three'
